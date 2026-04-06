1a7bd1dfbaac3b8f3e1f650c08734c14596e50b0
JYOTHISRI MUMMIDI <mummidijyothisri06@gmail.com>
2026-04-06 20:40:59 +0530

    latest bugfixes

--- server/src/utils/cache.js
+++ server/src/utils/cache.js
@@ -2,7 +2,16 @@ const { createClient } = require('redis');
 require('dotenv').config();
 
 const client = createClient({
-    url: process.env.REDIS_URL
+    url: process.env.REDIS_URL,
+    socket: {
+        reconnectStrategy: (retries) => {
+            if (retries > 3) {
+                console.error('Redis: Max retries reached. Shutting down reconnect strategy.');
+                return new Error('Max retries reached');
+            }
+            return Math.min(retries * 100, 3000);
+        }
+    }
 });
 
 client.on('error', (err) => console.log('Redis Client Error', err));
